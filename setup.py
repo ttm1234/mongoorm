3be30603927d5c797dbd6d49b5ba113c6cc0234a
ttm1234 <fake--dfafafaf@mail.com>
2019-08-04 17:42:17 +0800

    modify: modify version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='mongoorm',
-    version='1.0.1.dev2',
+    version='1.0.1.dev3',
     description='A mongodb orm python3 project',
     url='https://github.com/ttm1234/mongoorm',
     author='ttm1234',
