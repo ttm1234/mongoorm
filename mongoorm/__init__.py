59a80809e7d59f393c0a6f4aea7f7c3bb73389ae
ttm1234 <fake--dfafafaf@mail.com>
2019-11-09 18:16:17 +0800

    modify: change annotation

--- mongoorm/__init__.py
+++ mongoorm/__init__.py
@@ -10,7 +10,6 @@ from .meta import Meta
 1.查询和构造 model 时不做类型检查。只在保存时检查校验类型，如 .save 和 find_and_modify 和 find_one_and_update 等。
 2.查询到的 doc 如果 field 有 model 里尚未定义的则隐性保存并在 .save 时原样保存。
 
-todo find one and update 中对应 update 中的kv进行类型校验
 todo 创建 model 的 db_alias-collection 不能重复
 todo order_by('-age')
 todo 查询 filter_by[_one] 中支持 and or 等操作
@@ -20,6 +19,7 @@ todo aio_mongoorm
 todo 查询的语法改进Model.age >= 1
 todo pymongo 版本适配，当前 pymongo==3.7.2
 
+todo done!!!! find one and update 中对应 update 中的kv进行类型校验
 todo done!!!! 增加 filter_by[_one] 支持 age__gt=1 语法
 todo done!!!! self.to_json(), incloude ObjectId()
 todo done!!!! 支持 user = User(_id=1, name='xxx', ...)
